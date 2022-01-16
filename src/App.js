7203a5c31504831e12761e3eff0928ba0f788052
Braeae <91569703+Braeae@users.noreply.github.com>
2022-01-16 12:21:58 +0100

    React changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello React!</h1>
+        <Weather />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
